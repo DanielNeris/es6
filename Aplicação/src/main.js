41a456da26516e91e9466ed99638f102cdee9459
Daniel de Souza Neri <danielneris01@gmail.com>
2019-06-21 16:37:15 -0300

    Render em tela

--- Aplicação/src/main.js
+++ Aplicação/src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
@@ -21,7 +22,36 @@ class App {
             html_url: 'redcire'
         });
 
+        this.render();
+
         console.log(this.repositories);
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriprionEl = document.createElement('p');
+            descriprionEl.appendChild(document.createTextNode(repo.descriprion));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriprionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
